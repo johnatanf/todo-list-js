bf4f428bb5d2ef8638fff9a298648f78f877b4b7
johnatanf <johnatan.f100@gmail.com>
2024-05-30 11:41:08 +0700

    remove getFromLocalStorage function

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,7 @@ function refreshTodoContainerComponents () {
     let overdueData = [];
     let todayData = [];
     let doneData = [];
-    const localData = getDataFromLocalStorage();
+    const localData = retrieveDataFromLocalStorage();
     
     const overdueSection = document.getElementById('todo-overdue-container');
     const todaySection = document.getElementById('todo-today-container');
